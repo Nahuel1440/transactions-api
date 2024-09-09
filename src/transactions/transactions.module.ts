cc43f40add5a8c04e103a9e3a032559237dc83d5
Nahuel Toloza <Nahueltoloza014@gmail.com>
2024-09-08 21:23:46 -0300

    Fix - database and redis connection

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -16,6 +16,8 @@ import { UploadProcessor } from './processors/process.processor';
       connection: {
         host: process.env.REDIS_HOST,
         port: +process.env.REDIS_PORT,
+        username: process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD,
       },
     }),
     BullModule.registerQueue({
